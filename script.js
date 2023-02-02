c4b167b6de8e835c5cc7ead58e91b718e7701631
RoflanTR <96905621+RoflanTR@users.noreply.github.com>
2023-02-02 18:29:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ var popup = document.querySelector(".popup");
 var close = document.querySelector(".close");
 var body = document.querySelector("body");
 var html = document.querySelector("html");
+var widthWindow = window.screen.width;
 burgerMenu.addEventListener('click',function(){
     popup.style.display="block";
 })
 close.addEventListener('click',function(){
     popup.style.display="none";
 })
-
-burgerMenu.addEventListener('click', function(){
-    body.classList.add("open-menu");
-    html.classList.add("open-menu");
-})
-close.addEventListener('click', function(){
-    body.classList.remove("open-menu");
-    html.classList.remove("open-menu");
-})
+if(widthWindow<=821){
+    burgerMenu.addEventListener('click', function(){
+        body.classList.add("open-menu");
+        html.classList.add("open-menu");
+    })
+    close.addEventListener('click', function(){
+        body.classList.remove("open-menu");
+        html.classList.remove("open-menu");
+    })
+}
